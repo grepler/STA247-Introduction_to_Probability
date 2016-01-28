3a03843eee2d9f1ed7d14075bb98aca6992bbdd8
William Ellwood <grepler@ellwood.com>
2016-01-28 13:30:52 -0500

    Pythonic solutions to assorted questions from the first STA247 assignment

--- assignment1.py
+++ assignment1.py
@@ -1,14 +1,19 @@
 # question 4 (unit 2) number of odd seven digit integers with non-repeating
 # characters, using list comprehension from this week's class.
 
-lst = [x for x in range (0,9999999) for char in str(x) if (x % 2 == 1)
+def question1():
+
+    lst = [x for x in range (0,9999999) for char in str(x) if (x % 2 == 1)
        and str(x).count(char)==1]
-print(len(lst))
+    return(len(lst))
 
 # PyCharm CE can convert the list comprehension to for loop for cross-check
 
-lst = []
-for x in range(0, 9999999):
-    for char in str(x):
-        if (x % 2 == 1) and str(x).count(char) == 1:
-            lst.append(x)
+def question1_var():
+    lst = []
+    for x in range(0, 9999999):
+        for char in str(x):
+            if (x % 2 == 1) and str(x).count(char) == 1:
+                lst.append(x)
+    return(len(lst))
+
