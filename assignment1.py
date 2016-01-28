b9a57b7cb762fb035d5a34bac5be1ced9bf56737
William Ellwood <grepler@ellwood.com>
2016-01-28 13:28:52 -0500

    Pythonic solutions to assorted questions from the first STA247 assignment

--- assignment1.py
+++ assignment1.py
@@ -4,3 +4,11 @@
 lst = [x for x in range (0,9999999) for char in str(x) if (x % 2 == 1)
        and str(x).count(char)==1]
 print(len(lst))
+
+# PyCharm CE can convert the list comprehension to for loop for cross-check
+
+lst = []
+for x in range(0, 9999999):
+    for char in str(x):
+        if (x % 2 == 1) and str(x).count(char) == 1:
+            lst.append(x)
